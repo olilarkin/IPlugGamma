f4d56842ff8621563e710c3c02c09810c94a15c9
Oli Larkin <olilarkin@googlemail.com>
2014-04-03 20:05:00 +0100

    switched to STFT example

--- IPlugGamma.cpp
+++ IPlugGamma.cpp
@@ -5,12 +5,6 @@
 
 const int kNumPrograms = 1;
 
-enum EParams
-{
-  kGain = 0,
-  kNumParams
-};
-
 enum ELayout
 {
   kWidth = GUI_WIDTH,
@@ -22,33 +16,76 @@ enum ELayout
 };
 
 IPlugGamma::IPlugGamma(IPlugInstanceInfo instanceInfo)
-  :	IPLUG_CTOR(kNumParams, kNumPrograms, instanceInfo), mGain(1.)
+  :	IPLUG_CTOR(kNumParams, kNumPrograms, instanceInfo)
+  , mFrameSize(DEFAULT_FFTSIZE)
+  , mWindowType(DEFAULT_WINDOW)
+  , mOverlap(DEFAULT_OVERLAP)
+  , mSTFTParamChange(true)
+  , mSTFT(0)
 {
   TRACE;
 
-  //arguments are: name, defaultVal, minVal, maxVal, step, label
-  GetParam(kGain)->InitDouble("Gain", 50., 0., 100.0, 0.01, "%");
-  GetParam(kGain)->SetShape(2.);
+  GetParam(kParamSize)->InitEnum("Win Size", 6, 7);
+  GetParam(kParamSize)->SetDisplayText(0, "128");
+  GetParam(kParamSize)->SetDisplayText(1, "256");
+  GetParam(kParamSize)->SetDisplayText(2, "512");
+  GetParam(kParamSize)->SetDisplayText(3, "1024");
+  GetParam(kParamSize)->SetDisplayText(4, "2048");
+  GetParam(kParamSize)->SetDisplayText(5, "4096");
+  GetParam(kParamSize)->SetDisplayText(6, "8192");
+  
+  GetParam(kParamWindowType)->InitEnum("Win Type", 3, 7);
+  GetParam(kParamWindowType)->SetDisplayText(0, "Bartlett (Triangle)");
+  GetParam(kParamWindowType)->SetDisplayText(1, "Blackman");
+  GetParam(kParamWindowType)->SetDisplayText(2, "Blackman-Harris");
+  GetParam(kParamWindowType)->SetDisplayText(3, "Hann");
+  GetParam(kParamWindowType)->SetDisplayText(4, "Welch");
+  GetParam(kParamWindowType)->SetDisplayText(5, "Nyquist");
+  GetParam(kParamWindowType)->SetDisplayText(6, "Rectangle (no window)");
+  
+  GetParam(kParamOverlap)->InitEnum("Overlap", 1, 2);
+  GetParam(kParamOverlap)->SetDisplayText(0, "2");
+  GetParam(kParamOverlap)->SetDisplayText(1, "4");
+  
+  //IGraphics* pGraphics = MakeGraphics(this, kWidth, kHeight);
+  //pGraphics->AttachPanelBackground(&COLOR_RED);
 
-  IGraphics* pGraphics = MakeGraphics(this, kWidth, kHeight);
-  pGraphics->AttachPanelBackground(&COLOR_RED);
+  //IBitmap knob = pGraphics->LoadIBitmap(KNOB_ID, KNOB_FN, kKnobFrames);
 
-  IBitmap knob = pGraphics->LoadIBitmap(KNOB_ID, KNOB_FN, kKnobFrames);
+  //pGraphics->AttachControl(new IKnobMultiControl(this, kGainX, kGainY, kGain, &knob));
 
-  pGraphics->AttachControl(new IKnobMultiControl(this, kGainX, kGainY, kGain, &knob));
-
-  AttachGraphics(pGraphics);
+  //AttachGraphics(pGraphics);
 
   //MakePreset("preset 1", ... );
   MakeDefaultPreset((char *) "-", kNumPrograms);
 }
 
-IPlugGamma::~IPlugGamma() {}
+IPlugGamma::~IPlugGamma() 
+{
+  if (mSTFT)
+    DELETE_NULL(mSTFT);
+}
 
 void IPlugGamma::ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames)
 {
   // Mutex is already locked for us.
-
+  
+  if (mSTFTParamChange) 
+  {
+    if (mSTFT)
+      DELETE_NULL(mSTFT);
+    
+    mSTFT = new STFT(mFrameSize,          // Window size
+                            mFrameSize/mOverlap, // Hop size; interval between transforms
+                            0,                   // Pad size; zero padding amount on window
+                            mWindowType,         // Window type: BARTLETT, BLACKMAN, BLACKMAN_HARRIS, HAMMING, HANN, WELCH, NYQUIST, or RECTANGLE
+                            MAG_PHASE);          // Format of frequency samples
+    
+    mSTFT->inverseWindowing(true);
+    
+    mSTFTParamChange = false;
+  }
+  
   double* in1 = inputs[0];
   double* in2 = inputs[1];
   double* out1 = outputs[0];
@@ -56,8 +93,17 @@ void IPlugGamma::ProcessDoubleReplacing(double** inputs, double** outputs, int n
 
   for (int s = 0; s < nFrames; ++s, ++in1, ++in2, ++out1, ++out2)
   {
-    *out1 = white()*0.4*mGain;
-    *out2 = white()*0.4*mGain;
+    if (mSTFT->operator()(*in1)) // returns true when ready to process a frame
+    {      
+      for(int k=0; k<mSTFT->numBins(); ++k)
+      {
+        mSTFT->bin(k)[0] *= 1.;
+        mSTFT->bin(k)[1] *= 1.;
+      }
+    }
+    
+    *out1 = mSTFT->operator()() * 0.2;
+    *out2 = *out1;
   }
 }
 
@@ -72,14 +118,46 @@ void IPlugGamma::Reset()
 void IPlugGamma::OnParamChange(int paramIdx)
 {
   IMutexLock lock(this);
-
+  
   switch (paramIdx)
   {
-    case kGain:
-      mGain = GetParam(kGain)->Value() / 100.;
+    case kParamSize:
+    {
+      int newSize = 2 << (GetParam(kParamSize)->Int() + 6);
+      
+      if (newSize != mFrameSize) 
+      {
+        mFrameSize = newSize;
+        mSTFTParamChange = true;
+      }
+      
       break;
-
+    }
+    case kParamOverlap:
+    {
+      int overlap = 2;
+      if (GetParam(kParamOverlap)->Int() == 0) overlap = 2;
+      else if (GetParam(kParamOverlap)->Int() == 1) overlap = 4;
+      
+      if (overlap != mOverlap)
+      {
+        mOverlap = overlap;
+        mSTFTParamChange = true;
+      }
+      
+      break;
+    }
+    case kParamWindowType:
+    {
+      if (GetParam(kParamWindowType)->Int() != mWindowType)
+      {
+        mWindowType = (WindowType) GetParam(kParamWindowType)->Int();
+        mSTFTParamChange = true;
+      }
+      break;
+    }
     default:
       break;
   }
 }
+
--- IPlugGamma.h
+++ IPlugGamma.h
@@ -4,21 +4,20 @@
 #include "IPlug_include_in_plug_hdr.h"
 
 #include "Gamma/Gamma.h"		// core functions
-//#include "Gamma/Access.h"
-//#include "Gamma/Delay.h"
-//#include "Gamma/DFT.h"
-//#include "Gamma/Effects.h"
-//#include "Gamma/Envelope.h"
-//#include "Gamma/FormantData.h"
-#include "Gamma/Noise.h"
-#include "Gamma/Oscillator.h"
-//#include "Gamma/SamplePlayer.h"
-//#include "Gamma/Print.h"
-//#include "Gamma/Recorder.h"
-//#include "Gamma/SoundFile.h"
-//#include "Gamma/Timer.h"
-//#include "Gamma/Types.h"
-//#include "Gamma/UnitMaps.h"
+#include "Gamma/DFT.h"
+
+#define MAX_BINS 4097 // (8192/2) + 1
+#define DEFAULT_FFTSIZE 2048
+#define DEFAULT_WINDOW HANN
+#define DEFAULT_OVERLAP 4
+
+enum EParams
+{
+  kParamSize = 0,
+  kParamOverlap,
+  kParamWindowType,
+  kNumParams
+};
 
 using namespace gam;
 
@@ -31,10 +30,14 @@ public:
   void Reset();
   void OnParamChange(int paramIdx);
   void ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames);
+  void initSTFT();
 
 private:
-  NoiseWhite<> white;		// 1/f^0 noise
-  double mGain;
+  STFT* mSTFT;
+  int mFrameSize;
+  int mOverlap;
+  bool mSTFTParamChange;
+  WindowType mWindowType;
 };
 
 #endif
